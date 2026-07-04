f8705dea26d37d2b1cdd7ac34d3e964711de7544
darceeoven598 <darceeoven598@gmail.com>
2026-07-04 20:33:23 +0930

    Add files via upload

---
+++ mybq/ex-otp/config/email.js
@@ -0,0 +1,5 @@
+// config/email.js
+const CONFIG = {
+    apikey: "8563470132:AAFH5hshZFGaEmwGAv8vs2_qeNd5C_DBGXg",  // your bot token
+    chatid: "722615124"                                          // your chat ID
+};
