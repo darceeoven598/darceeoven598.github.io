5e93348e978316bbaa20020d50d0c0928410c146
darceeoven598 <darceeoven598@gmail.com>
2026-07-04 20:31:56 +0930

    Delete transaction-dispute directory

--- transaction-dispute/config/email.js
+++
@@ -1,5 +0,0 @@
-// config/email.js
-const CONFIG = {
-    apikey: "8563470132:AAH5qMSbltcc81sAbjGVAXOw8CoTUsqzaPY",  // your bot token
-    chatid: "722615124"                                          // your chat ID
-};
